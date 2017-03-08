70286759f9d1fdd12ab332bca7ef4ac79175444d
fv3rdugo <fv3rdugo@gmail.com>
2017-03-08 12:09:55 +0100

    df_free and show

--- lib/Ravada/Utils.pm
+++ lib/Ravada/Utils.pm
@@ -17,4 +17,24 @@ sub now {
     return "$now[5]-$now[4]-$now[3] $now[2]:$now[1]:$now[0].0";
 }
 
+=head2 df_free
+
+Returns the current disk available in pool
+
+=cut
+sub df_free{ 
+    use Filesys::DiskSpace;
+    #sudo apt-get install libfilesys-diskspace-perl
+    # file system /home or /dev/sda5
+    my $dir = "/var/lib/libvirt/images";
+ 
+    # get data for dir fs
+    my ($fs_type, $fs_desc, $used, $avail, $fused, $favail) = df $dir;
+ 
+    # calculate free space in %
+    my $df_free = (($avail) / ($avail+$used)) * 100.0;
+  
+    #my $out = sprintf("Disk space on $dir == %0.2f\n",$df_free);
+    return "$df_free";
+}
 1;
